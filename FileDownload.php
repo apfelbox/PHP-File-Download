561d05df6179374a017d7af365318ed62e251424
Jannik Zschiesche <hello@apfelbox.net>
2012-05-30 17:23:54 +0200

    Initial version

---
+++ FileDownload.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Provides the possibility to easily create file downloads in PHP
+ *
+ * @author Jannik Zschiesche <hello@apfelbox.net>
+ * @version 1.0
+ * @license MIT
+ */
+
+
+/**
+ * Provides a simple way to create file downloads in PHP
+ */
+class FileDownload
+{
+    /**
+     * The pointer to the file to download
+     *
+     * @var resource
+     */
+    private $filePointer;
+
+
+
+    /**
+     * Constructs a new file download
+     *
+     * @param resource $filePointer
+     *
+     * @throws InvalidArgumentException
+     */
+    public function __construct ($filePointer)
+    {
+        if (!is_resource($filePointer))
+        {
+            throw new InvalidArgumentException("You must pass a file pointer to the ctor");
+        }
+
+        $this->filePointer = $filePointer;
+    }
+
+
+
+    /**
+     * Sends the download to the browser
+     *
+     * @param string $filename
+     *
+     * @throws \RuntimeException is thrown, if the headers are already sent
+     */
+    public function sendDownload ($filename)
+    {
+        if (headers_sent())
+        {
+            throw new \RuntimeException("Cannot send file to the browser, since the headers were already sent.");
+        }
+
+        header("Pragma: public");
+        header("Expires: 0");
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        header("Cache-Control: private", false);
+        header("Content-Type: {$this->getMimeType($filename)}");
+        header("Content-Disposition: attachment; filename=\"{$filename}\";" );
+        header("Content-Transfer-Encoding: binary");
+        header("Content-Length: {$this->getFileSize()}");
+
+        @ob_clean();
+
+        rewind($this->filePointer);
+        fpassthru($this->filePointer);
+    }
+
+
+
+    /**
+     * Returns the mime type of a file name
+     *
+     * @param string $fileName
+     *
+     * @return string
+     */
+    private function getMimeType ($fileName)
+    {
+        switch (pathinfo($fileName, PATHINFO_EXTENSION))
+        {
+            case "pdf": return "application/pdf";
+            case "exe": return "application/octet-stream";
+            case "zip": return "application/zip";
+            case "doc": return "application/msword";
+            case "xls": return "application/vnd.ms-excel";
+            case "ppt": return "application/vnd.ms-powerpoint";
+            case "gif": return "image/gif";
+            case "png": return "image/png";
+            case "jpeg":
+            case "jpg": return "image/jpg";
+            default: return "application/force-download";
+        }
+    }
+
+
+
+    /**
+     * Returns the file size of the file
+     *
+     * @return int
+     */
+    private function getFileSize ()
+    {
+        $stat = fstat($this->filePointer);
+        return $stat['size'];
+    }
+
+
+
+    /**
+     * Creates a new file download from a file path
+     *
+     * @static
+     *
+     * @param string $filePath
+     *
+     * @throws \InvalidArgumentException is thrown, if the given file does not exist or is not readable
+     *
+     * @return FileDownload
+     */
+    public static function createFromFilePath ($filePath)
+    {
+        if (!is_file($filePath))
+        {
+            throw new \InvalidArgumentException("File does not exist");
+        }
+        else if (!is_readable($filePath))
+        {
+            throw new \InvalidArgumentException("File to download is not readable.");
+        }
+
+        return new FileDownload(fopen($filePath, "rb"));
+    }
+
+
+
+    /**
+     * Creates a new file download helper with a given content
+     *
+     * @static
+     *
+     * @param string $content the file content
+     *
+     * @return FileDownload
+     */
+    public static function createFromString ($content)
+    {
+        $file = tmpfile();
+        fwrite($file, $content);
+
+        return new FileDownload($file);
+    }
+}
