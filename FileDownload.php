6dd2e5cd043b7c5e5293431234f6e7496d6e579a
Rob Lambell <rob@lambell.info>
2014-01-19 04:32:14 +0000

    Added MIME types, using array instead of switch. Amended formatting.

--- FileDownload.php
+++ FileDownload.php
@@ -20,6 +20,84 @@ class FileDownload
      */
     private $filePointer;
 
+    /**
+     * Mime types
+     *
+     * @var array
+     */
+    private $mimeTypes = array(
+        '7z' => 'application/octet-stream',
+        'ai' => 'application/illustrator',
+        'avi' => 'video/x-msvideo',
+        'bmp' => 'image/bmp',
+        'cab' => 'application/vnd.ms-cab-compressed',
+        'css' => 'text/css',
+        'diff' => 'text/x-patch',
+        'doc' => 'application/msword',
+        'docm' => 'application/vnd.ms-word.document.macroEnabled.12',
+        'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'dot' => 'application/msword',
+        'dotm' => 'application/vnd.ms-word.template.macroEnabled.12',
+        'dotx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
+        'eps' => 'application/postscript',
+        'exe' => 'application/x-msdownload',
+        'flv' => 'video/x-flv',
+        'gif' => 'image/gif',
+        'htm' => 'text/html',
+        'html' => 'text/html',
+        'ico' => 'image/vnd.microsoft.icon',
+        'jpe' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'jpg' => 'image/jpeg',
+        'js' => 'application/javascript',
+        'json' => 'application/json',
+        'mov' => 'video/quicktime',
+        'mp3' => 'audio/mpeg',
+        'mpe' => 'video/mpeg',
+        'mpeg' => 'video/mpeg',
+        'mpg' => 'video/mpeg',
+        'msi' => 'application/x-msdownload',
+        'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+        'odt' => 'application/vnd.oasis.opendocument.text',
+        'pdf' => 'application/pdf',
+        'php' => 'text/html',
+        'png' => 'image/png',
+        'pot' => 'application/vnd.ms-powerpoint',
+        'potm' => 'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+        'potx' => 'application/vnd.openxmlformats-officedocument.presentationml.template',
+        'ppa' => 'application/vnd.ms-powerpoint',
+        'ppam' => 'application/vnd.ms-powerpoint.addin.macroEnabled.12',
+        'pps' => 'application/vnd.ms-powerpoint',
+        'ppsm' => 'application/vnd.ms-powerpoint.slideshow.macroEnabled.12',
+        'ppsx' => 'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+        'ppt' => 'application/vnd.ms-powerpoint',
+        'pptm' => 'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+        'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+        'ps' => 'application/postscript',
+        'psd' => 'image/vnd.adobe.photoshop',
+        'qt' => 'video/quicktime',
+        'rar' => 'application/x-rar-compressed',
+        'rtf' => 'application/rtf',
+        'svg' => 'image/svg+xml',
+        'svgz' => 'image/svg+xml',
+        'swf' => 'application/x-shockwave-flash',
+        'tif' => 'image/tiff',
+        'tiff' => 'image/tiff',
+        'txt' => 'text/plain',
+        'wav' => 'audio/x-wav',
+        'xla' => 'application/vnd.ms-excel',
+        'xlam' => 'application/vnd.ms-excel.addin.macroEnabled.12',
+        'xls' => 'application/vnd.ms-excel',
+        'xlsb' => 'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
+        'xlsm' => 'application/vnd.ms-excel.sheet.macroEnabled.12',
+        'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'xlt' => 'application/vnd.ms-excel',
+        'xltm' => 'application/vnd.ms-excel.template.macroEnabled.12',
+        'xltx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
+        'xml' => 'application/xml',
+        'zip' => 'application/zip',
+    );
+
 
 
     /**
@@ -45,7 +123,7 @@ class FileDownload
      * Sends the download to the browser
      *
      * @param string $filename
-	 * @param bool $forceDownload
+     * @param bool $forceDownload
      *
      * @throws \RuntimeException is thrown, if the headers are already sent
      */
@@ -61,11 +139,16 @@ class FileDownload
         header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
         header("Cache-Control: private", false);
         header("Content-Type: {$this->getMimeType($filename)}");
-        if ($forceDownload) {
+
+        if ($forceDownload)
+        {
             header("Content-Disposition: attachment; filename=\"{$filename}\";" );
-        }else{
+        }
+        else
+        {
             header("Content-Disposition: filename=\"{$filename}\";" );
         }
+
         header("Content-Transfer-Encoding: binary");
         header("Content-Length: {$this->getFileSize()}");
 
@@ -86,19 +169,15 @@ class FileDownload
      */
     private function getMimeType ($fileName)
     {
-        switch (pathinfo($fileName, PATHINFO_EXTENSION))
+        $extension = pathinfo($fileName, PATHINFO_EXTENSION);
+
+        if (isset($this->mimeTypes[$extension]))
         {
-            case "pdf": return "application/pdf";
-            case "exe": return "application/octet-stream";
-            case "zip": return "application/zip";
-            case "doc": return "application/msword";
-            case "xls": return "application/vnd.ms-excel";
-            case "ppt": return "application/vnd.ms-powerpoint";
-            case "gif": return "image/gif";
-            case "png": return "image/png";
-            case "jpeg":
-            case "jpg": return "image/jpg";
-            default: return "application/force-download";
+            return $this->mimeTypes[$extension];
+        }
+        else
+        {
+            return "application/force-download";
         }
     }
 
